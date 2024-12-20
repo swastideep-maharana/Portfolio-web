004ed9556d3c61782503d51362d9d2d4838bb70a
swastideep maharana <swastideep67maharana@gmail.com>
2024-12-20 20:35:06 +0530

    #did the fix in the hero sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/profile-pic.jpg";
+import profilePic from "../assets/swastideepProfile.png"; // Correct file name and extension
 
 import { motion, useInView } from "framer-motion";
 
